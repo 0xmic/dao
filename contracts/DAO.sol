e659ba7c8f149a53779015369e146f65ae7f17e4
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-05-10 21:26:11 -0700

    Add createProposal fxn

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -5,17 +5,62 @@ import "hardhat/console.sol";
 import './Token.sol';
 
 contract DAO {
-  address owner;
-  Token public token;
-  uint256 public quorum;
+    address owner;
+    Token public token;
+    uint256 public quorum;
 
-  constructor(Token _token, uint256 _quorum) {
-    owner = msg.sender;
-    token = _token;
-    quorum = _quorum;
-  }
+    struct Proposal {
+		uint256 id;
+        string name;
+        uint256 amount;
+        address payable recipient;
+		uint256 votes;
+		bool finalized;
+    }
 
-  // Allow contract to receive ether
-  receive() external payable {}
+	uint256 public proposalCount;
+	mapping(uint256 => Proposal) public proposals;
 
+	event Propose(
+		uint id,
+		uint256 amount,
+		address recipient,
+		address creator
+	);
+
+    constructor(Token _token, uint256 _quorum) {
+        owner = msg.sender;
+        token = _token;
+        quorum = _quorum;
+    }
+
+    // Allow contract to receive ether
+    receive() external payable {}
+
+	modifier onlyInvestor() {
+		require(Token(token).balanceOf(msg.sender) > 0, "Must be token holder");
+		_;
+	}
+
+	// Create proposal
+    function createProposal(
+        string memory _name,
+        uint256 _amount,
+        address payable _recipient
+    ) external onlyInvestor {
+		require(address(this).balance >= _amount, "Not enough funds");
+
+		proposalCount++;
+
+		proposals[proposalCount] = Proposal(
+			proposalCount,
+			_name,
+			_amount,
+			_recipient,
+			0,
+			false
+		);
+
+		emit Propose(proposalCount, _amount, _recipient, msg.sender);
+	}
 }
